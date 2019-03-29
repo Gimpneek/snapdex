0b7bf22f3d1d55e4aab7713d11983f04b5b6f7f3
Gimpneek <colin@gimpneek.com>
2019-03-29 20:03:07 +0000

    - Using Pokemon list to power the bot

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import os
 import logging
 import csv
 from snapdex.snapdexClient import SnapdexClient
+from snapdex.pokemonListItem import PokemonListItem
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
@@ -18,8 +19,19 @@ all_pokemon = []
 
 with open('pokemon.csv', 'r') as f:
     reader = csv.reader(f)
-    for raw_pokemon in list(reader)[1:]:
-        all_pokemon.append(raw_pokemon[1].lower())
+    for pokemon in list(reader)[1:]:
+        all_pokemon.append(
+            PokemonListItem(
+                name=pokemon[1],
+                alias_list=pokemon[9].split('|'),
+                number=pokemon[0],
+                types=pokemon[2].split('|'),
+                dex_entries=pokemon[8].split('|'),
+                sprite=pokemon[7],
+                legendary=pokemon[4],
+                mythic=pokemon[5],
+                regional=pokemon[10])
+        )
 
 all_pokemon = set(all_pokemon)
 
