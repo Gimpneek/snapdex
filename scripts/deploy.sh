771917b4e1017b282752cc3e9e17e053475e7bda
Gimpneek <colin@gimpneek.com>
2019-05-05 00:04:21 +0100

    - Hoping this fixes issue with env vars not being passed to build_docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,4 @@
-# install AWS SDK
-pip install awscli
-export PATH=$PATH:$HOME/.local/bin
-
-# install necessary dependency for ecs-deploy
-sudo add-apt-repository ppa:eugenesan/ppa -y
-sudo apt-get update -y
-sudo apt-get install jq -y
-
-# Login DockerHub
-echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USER --password-stdin
-
 # build the docker image and push to an image repository
-docker build -t $IMAGE_REPO_URL:$TRAVIS_TAG .
 docker tag $IMAGE_REPO_URL:$TRAVIS_TAG $IMAGE_REPO_URL:$TRAVIS_TAG
 docker push $IMAGE_REPO_URL:$TRAVIS_TAG
 
