4bceca5dbd8650acdea9979c557888d2118a6d3f
Gimpneek <colin@gimpneek.com>
2019-05-04 23:01:25 +0100

    - Adding additional checks

--- docker/test_files.py
+++ docker/test_files.py
@@ -4,6 +4,9 @@ import pytest
 
 @pytest.mark.usefixtures("host")
 class TestRequirements(TestCase):
+    """
+    Check that the files needed to run the bot are in the Docker image
+    """
 
     def setUp(self):
         super(TestRequirements, self).setUp()
@@ -11,7 +14,15 @@ class TestRequirements(TestCase):
         self.bot = self.host.file('/src/main.py')
 
     def test_pokemon_list(self):
+        """
+        Check that the pokemon.csv file needed to load in the Pokemon data
+        is present in the image
+        """
         self.assertTrue(self.pokemon.exists)
 
     def test_bot(self):
+        """
+        Check that the main.py file used to run the bot is present in the
+        image
+        """
         self.assertTrue(self.bot.exists)
